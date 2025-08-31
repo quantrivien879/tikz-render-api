208b3002ba20cf6b22ad24960356f458d8f8e821
quantrivien879 <quantrivien879@gmail.com>
2025-09-01 06:07:37 +0700

    Update tex_templates.py

--- app/tex_templates.py
+++ app/tex_templates.py
@@ -1,7 +1,10 @@
 BASE_PREAMBLE = r"""
 \documentclass[tikz]{standalone}
 \usepackage{amsmath, amssymb}
-\usepackage{tikz}
+\usepackage{tikz,tkz-tab}
+\usetikzlibrary{shapes,shapes.geometric,arrows,calc,intersections,angles,patterns,arrows,decorations.pathmorphing,backgrounds,positioning,fit,petri,shapes.symbols,matrix,tikzmark}
+\usetikzlibrary{positioning,decorations.text,decorations.pathmorphing}% Để uốn cong văn bản
+\usetikzlibrary{shadings,fadings} %ĐỔ BÓNG
 % --- Add-ons here ---
 {EXTRA_PACKAGES}
 {EXTRA_PREAMBLE}
